ed1d4c5d517d861d136e9981744c5197f801d28b
Vaishnavi <vaishnavi.rj10@gmail.com>
2025-04-19 09:07:04 +0530

    first commit

---
+++ tokens.py
@@ -0,0 +1,21 @@
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+#Load a pretrained model and a tokenizer using HuggingFace
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+
+# tokenize a sammple sentence
+text = "The cow jumped over the moon"
+print(text)
+inputs = tokenizer(text, return_tensors="pt")
+
+# print the tokenized input
+print(inputs)
+
+print(inputs["input_ids"]) # print the token ids
+
+
+# decode the tokenized input
+decoded_text = tokenizer.decode(inputs["input_ids"][0])
+print(decoded_text) 
