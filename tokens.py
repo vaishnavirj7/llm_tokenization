8a87af9c736e74883feb926f1aafd5ca3bd1c2cf
Vaishnavi <vaishnavi.rj10@gmail.com>
2025-04-20 22:59:19 +0530

    added a function

--- tokens.py
+++ tokens.py
@@ -1,21 +1,25 @@
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-#Load a pretrained model and a tokenizer using HuggingFace
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelForCausalLM.from_pretrained("gpt2")
+def tokenize():
+    #Load a pretrained model and a tokenizer using HuggingFace
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    model = AutoModelForCausalLM.from_pretrained("gpt2")
 
-# tokenize a sammple sentence
-text = "The cow jumped over the moon"
-print(text)
-inputs = tokenizer(text, return_tensors="pt")
+    # tokenize a sammple sentence
+    text = "The cow jumped over the moon"
+    print(text)
+    inputs = tokenizer(text, return_tensors="pt")
 
-# print the tokenized input
-print(inputs)
+    # print the tokenized input
+    print(inputs)
 
-print(inputs["input_ids"]) # print the token ids
+    print(inputs["input_ids"]) # print the token ids
 
 
-# decode the tokenized input
-decoded_text = tokenizer.decode(inputs["input_ids"][0])
-print(decoded_text) 
+    # decode the tokenized input
+    decoded_text = tokenizer.decode(inputs["input_ids"][0])
+    print(decoded_text)
+
+if __name__ == "__main__":
+    tokenize()
